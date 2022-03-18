39c9562a27d8a8c09d7b9d0733f1ced6b31805bb
Luan Maidana Lima <luanmaidanalima_@hotmail.com>
2022-03-18 00:38:27 -0400

    Configurando jwt

---
+++ Extensions/AppSettings.cs
@@ -0,0 +1,11 @@
+namespace MyPlace.Extensions
+{
+    public class AppSettings{
+
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,17 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Myplace.Data;
 using MyPlace.Data;
+using MyPlace.Extensions;
 using MyPlace.Negocios;
 using Newtonsoft.Json.Serialization;
 
@@ -17,7 +22,15 @@ namespace MyPlace
 
         
         // This method gets called by the runtime. Use this method to add services to the container.
+        
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+        public Startup(IConfiguration configuration){
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {        
 
@@ -28,6 +41,32 @@ namespace MyPlace
                     .AddRoles<IdentityRole>()
                     .AddEntityFrameworkStores<MyPlaceDbContext>()
                     .AddDefaultTokenProviders();
+
+            // JWT configuração
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(options => {
+
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(bearerOptions => {
+
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters{
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("x")),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
 
             services.AddControllers();
             
